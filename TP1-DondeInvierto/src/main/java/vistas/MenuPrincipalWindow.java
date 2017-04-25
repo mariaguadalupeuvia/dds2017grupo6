532bfe5ca695875cb053eb5ef0b78edc11b13c37
ggallici <gallicig94@gmail.com>
2017-04-25 13:05:43 -0300

    Template basico del proyecto

---
+++ TP1-DondeInvierto/src/main/java/vistas/MenuPrincipalWindow.java
@@ -0,0 +1,49 @@
+package vistas;
+
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+
+import vistas.carga.cuenta.MenuCargaCuentasWindow;
+import vistas.consulta.cuenta.ConsultaCuentaWindow;
+import vm.MenuPrincipalVM;
+
+@SuppressWarnings("serial")
+public class MenuPrincipalWindow extends SimpleWindow<MenuPrincipalVM>
+{
+	public MenuPrincipalWindow(WindowOwner parent) {
+		
+		super(parent, new MenuPrincipalVM());
+	}
+
+	@Override
+	protected void createFormPanel(Panel panelPrincipal) 
+	{
+		panelPrincipal.setLayout(new HorizontalLayout());
+		
+		this.setTitle("Menu");
+	}
+	
+	@Override
+	protected void addActions(Panel botonera) 
+	{
+		botonera.setLayout(new VerticalLayout());
+		
+		new Button(botonera).setCaption("1. Carga de cuentas").onClick(this::carga).setWidth(200);
+
+		new Button(botonera).setCaption("2. Consulta de cuentas").onClick(this::consulta).setWidth(200);
+	}
+	
+	public void carga()
+	{
+		SimpleWindow<?> ventana = new MenuCargaCuentasWindow(this);
+		ventana.open();
+	}
+	public void consulta(){
+		SimpleWindow<?> ventana = new ConsultaCuentaWindow(this);
+		ventana.open();
+	}
+}
---
+++ TP1-DondeInvierto/src/main/java/vm/consulta/cuenta/ConsultaCuentaVM.java
@@ -0,0 +1,6 @@
+package vm.consulta.cuenta;
+
+public class ConsultaCuentaVM 
+{
+
+}
