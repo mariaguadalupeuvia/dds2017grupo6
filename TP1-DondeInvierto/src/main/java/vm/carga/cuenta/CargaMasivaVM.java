bb6167b8e07a2e7855a07f90b1fae456831bfa31
Jose A.Serrano <josealberto117@gmail.com>
2017-04-27 01:29:39 -0300

    carga excel
    se agrega una pequeña sintaxis de uso para automatizacion excel

--- TP1-DondeInvierto/src/main/java/vm/carga/cuenta/CargaMasivaVM.java
+++ TP1-DondeInvierto/src/main/java/vm/carga/cuenta/CargaMasivaVM.java
@@ -1,5 +1,29 @@
 package vm.carga.cuenta;
 
-public class CargaMasivaVM {
+import dominio.DatosExcelCargaMasiva;
+import dominio.ProcesarArchivoExcel;
 
+public class CargaMasivaVM {
+	private String nombredelArchivo;
+	
+	public void Procesar(){
+		String rutaArchivo = "C:/Users/HUCHIHA/Documents/prueba_excel.xls";
+		DatosExcelCargaMasiva resultado = new DatosExcelCargaMasiva(); 
+		resultado = ProcesarArchivoExcel.CargarInformaciondeExcel(rutaArchivo);
+		System.out.print(resultado.empresas().size());
+		resultado.empresas().forEach(empresa->System.out.print(empresa));
+		System.out.print(resultado.periodos().size());
+		resultado.periodos().forEach(periodo->System.out.print(periodo));
+		System.out.print(resultado.cuentas().size());
+		resultado.cuentas().forEach(cuenta->System.out.print(cuenta));
+		System.out.print(resultado.getRegistros().size());
+	}
+
+	public String getNombredelArchivo() {
+		return nombredelArchivo;
+	}
+
+	public void setNombredelArchivo(String nombredelArchivo) {
+		this.nombredelArchivo = nombredelArchivo;
+	}
 }
